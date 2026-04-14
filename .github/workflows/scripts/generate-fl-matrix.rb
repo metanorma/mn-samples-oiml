b7b1b7726acf9b2b782852ad276a4f9b8b92dab1
Ronald Tse <ronald.tse@ribose.com>
2026-04-14 09:36:06 +0800

    feat: integrate Firelight HTML builds into generate.yml workflow

--- .github/workflows/scripts/generate-fl-matrix.rb
+++ .github/workflows/scripts/generate-fl-matrix.rb
@@ -11,40 +11,53 @@ SOURCES_DIR = 'sources'
 config = YAML.load_file(MANIFEST)
 files = config.dig('metanorma', 'source', 'files') || []
 
+# Determine collection names from metanorma.yml to exclude sub-documents
+collection_names = files
+  .map { |entry| entry.is_a?(Hash) ? entry['files'] : entry }
+  .select { |src| src =~ /collection\.yml\z/ }
+  .map { |src| File.basename(File.dirname(src)) }
+
 prefix = '/mn-samples-oiml/'
 
 entries = files.map do |entry|
   src = entry.is_a?(Hash) ? entry['files'] : entry
   parent_dir = File.dirname(src)          # e.g. "sources/r144/1" or "sources/b022-e23"
-  grandchild_dir = File.basename(parent_dir)  # e.g. "1" or "b022-e23"
-  grandparent_dir = File.basename(File.dirname(parent_dir))  # e.g. "r144" or "sources"
-  name = File.basename(parent_dir)
+  name = File.basename(parent_dir)       # e.g. "1", "b022-e23", "main"
+  grandparent_dir = File.basename(File.dirname(parent_dir))  # e.g. "r144", "sources"
   is_collection = src =~ /collection\.yml\z/
-
-  # Handle subdocuments: sources/r144/1/document.adoc -> name "r144-1"
-  if grandparent_dir == 'r144' && name =~ /\A\d+\z/
-    name = "r144-#{name}"
-  end
 
   if is_collection
     {
       'name' => name,
-      'artifact' => "mn-#{name}",
-      'dir' => '',
+      'artifact' => 'mn',
+      'dir' => 'documents/collection-output',
       'xml' => 'collection.presentation.xml',
       'url' => name,
       'prefix' => prefix
     }
-  else
+  elsif collection_names.include?(grandparent_dir)
+    # Subdocument within a collection
+    collection_name = grandparent_dir
+    url = "#{collection_name}/#{name}"
     {
-      'name' => name,
+      'name' => url.gsub('/', '-'),
       'artifact' => 'mn',
-      'dir' => "documents/#{parent_dir.sub('sources/', '')}",
+      'dir' => "documents/#{collection_name}",
+      'xml' => "#{name}/document.presentation.xml",
+      'url' => url,
+      'prefix' => prefix
+    }
+  else
+    url = parent_dir.sub('sources/', '')
+    {
+      'name' => url.gsub('/', '-'),
+      'artifact' => 'mn',
+      'dir' => "documents/#{url}",
       'xml' => 'document.presentation.xml',
-      'url' => name,
+      'url' => url,
       'prefix' => prefix
     }
   end
-end
+end.compact
 
 puts entries.to_json
